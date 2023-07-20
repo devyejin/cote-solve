803745123abe28f3f8bb625be19fd0537a31da2c
yejin <yejinoh58@gmail.com>
2023-07-20 11:31:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            long sum = 0;
+            while(st.hasMoreTokens()) {
+                sum += Long.parseLong(st.nextToken());
+            }
+
+            System.out.println(sum);
+
+        }
+    }
+}
