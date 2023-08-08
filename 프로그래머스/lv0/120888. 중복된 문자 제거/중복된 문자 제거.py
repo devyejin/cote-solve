87c1dfe4fdfa6ee5be1a435339adb8faf8e1e930
yejin <yejinoh58@gmail.com>
2023-08-08 15:15:30 +0900

    Update 중복된 문자 제거.py

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -6,3 +6,8 @@ def solution(my_string):
             answer += char
             
     return answer
+
+
+#좀 더 효율적인 풀이법
+#     딕셔너리의 key가 중복허용되지 않는점을 이용한 후 join으로 문자열 반환
+#    return ''.join(dict.fromkeys(my_string))
