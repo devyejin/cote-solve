495eac339d181c19dc9e1d577aeda704182b90a5
yejin <yejinoh58@gmail.com>
2023-09-19 01:24:50 +0900

    Update 자연수 뒤집어 배열로 만들기.py

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -6,3 +6,12 @@ def solution(n):
     reversed_list = list(map(int,reversed(str_n)))
     
     return reversed_list
+
+# 다른사람 풀이
+# 꼭 함수명이 solution일 필요 없음, 간결하게 한 줄로 
+def digit_reverse(n):
+    return list(map(int,reversed(str(n))))
+
+#
+def digit_reverse(n):
+    return [int(i) for i in str(n)][::-1]  #숫자 배열로 만든다음 뒤집기
