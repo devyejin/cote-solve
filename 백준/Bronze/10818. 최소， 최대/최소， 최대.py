73a2278cfa7f816135e9a49ca549f57f4e7d0bfa
yejin <yejinoh58@gmail.com>
2023-09-20 03:18:43 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 147552 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,16 @@
+# 마지막 요소 -2 고려하면 좋은 코드는 아닌거같음
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+#
+# numbers = list(map(int,input().split()))
+# numbers.sort()
+#
+# print(numbers.pop(0),numbers.pop(N-2))
+
+# 반례를 생각한다면, element가 2개밖에 없는 경우 최소,최대는 있는데 이 로직은 못써먹음
+
 import sys
 input = sys.stdin.readline
 
