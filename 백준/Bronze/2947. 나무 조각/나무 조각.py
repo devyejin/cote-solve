5174fa26d55828290a7db2cb22c986aed28cecc9
yejin <yejinoh58@gmail.com>
2023-09-21 03:03:22 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -1,25 +1,11 @@
-numbers = list(map(int, input().split())) # 데이터 받고
+import sys
+input = sys.stdin.readline
 
-while True:
-    # idx옮겨가며 비교, 왼 > 오 자리 변경
+woods = list(map(int, input().split()))
 
-    total_sorted = False
+while woods != [1, 2, 3, 4, 5]:
 
-    for idx in range(len(numbers)-1):
-
-        if numbers[idx] > numbers[idx+1]:
-            numbers[idx], numbers[idx+1] = numbers[idx+1], numbers[idx] #크
-            for num in numbers:
-                print(num, end=" ")
-
-            print()
-        #numbers.sort() 는 원본을 정렬하고, 반환은 None
-        #sorted() 사용해야 새로운 객체 반환
-        temp_numbers = sorted(numbers)
-        if numbers == temp_numbers:
-            total_sorted = True
-
-    if total_sorted:
-        break
-
-        # 다 돌았는데도 정렬이 안됐다면 다시 반복해야되는데 => while문 이용
+    for i in range(1,5):
+        if woods[i - 1] > woods[i]:
+            woods[i -1] , woods[i] = woods[i], woods[i -1]
+            print(*woods) # * : 리스트 요소들을 언패킹해서 출력 즉, [1,2,3,4,5] => 1 2 3 4 5로 출력해줌
