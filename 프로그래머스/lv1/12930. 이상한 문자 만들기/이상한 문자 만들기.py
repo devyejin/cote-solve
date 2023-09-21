d5f556087a20bd8f664f2248505c2f26ad591f1f
yejin <yejinoh58@gmail.com>
2023-09-22 01:58:17 +0900

    이상한 문자 만들기.py

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -8,4 +8,22 @@ def solution(s):
             else:
                 answer += i[j].lower()
         answer+= ' '
-    return answer[0:-1]
+    return answer[0:-1] #마지막 단어에서도 ' ' 공백이 붙어버려서 제거하고 출력
+
+
+# 기존 풀이인데, 이 방식은 테스트케이스 일부가 통과 못함
+# 제한사항에! 문자열 전체의 인덱스가 아닌 !!단어별 인덱스!!
+def solution(s):
+    count = 1
+    answer = []
+    for char in s:
+
+        if count % 2 == 1:
+            answer.append(char.upper())
+        else:
+            answer.append(char.lower())
+
+        count += 1 ## 파이썬은 count++ 미지원
+
+    return ''.join(answer) # 리스트를 문자열로 반환하는 방법
+
