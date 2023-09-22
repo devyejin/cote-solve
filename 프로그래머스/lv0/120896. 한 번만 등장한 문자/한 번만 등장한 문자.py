71b581f1b0f9f50c7f755289d7ee876d408310d9
yejin <yejinoh58@gmail.com>
2023-09-22 18:01:57 +0900

    한 번만 등장한 문자.py

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -13,3 +13,19 @@ def solution(s):
             answer.append(key)
 
     return ''.join(sorted(answer))
+
+
+
+# 리스트안에 특정 요소가 몇 개 들어있는지 알려주는 메서드 활용한 풀이 방법 list.count(element)
+def solution(s):
+    letters = list(s)
+    answer_list = []
+
+    for letter in letters:
+        if letters.count(letter) == 1:
+            answer_list.append(letter)
+
+    answer_list.sort()
+    answer = ''.join(answer_list)
+    
+    return answer
