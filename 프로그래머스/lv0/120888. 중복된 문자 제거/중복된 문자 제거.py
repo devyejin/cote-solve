bbf624fb56377f9d5249c5b7d368d7a770af1e6b
yejin <yejinoh58@gmail.com>
2023-09-22 17:24:52 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,13 +1,8 @@
 def solution(my_string):
-    answer = ''
-    
+    answer = []
+
     for char in my_string:
         if char not in answer:
-            answer += char
-            
-    return answer
+            answer.append(char)
 
-
-#좀 더 효율적인 풀이법
-#     딕셔너리의 key가 중복허용되지 않는점을 이용한 후 join으로 문자열 반환
-#    return ''.join(dict.fromkeys(my_string))
+    return ''.join(answer)
