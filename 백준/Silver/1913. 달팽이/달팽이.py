fab00f3bce8a24a593acecec268bdb17eeb66353
yejin <yejinoh58@gmail.com>
2024-04-20 18:51:05 +0900

    [Silver III] Title: 달팽이, Time: 972 ms, Memory: 70032 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,26 +1,29 @@
-import sys
-input = sys.stdin.readline
-
 dr = (1, 0, -1, 0)
 dc = (0, 1, 0, -1)
 
 N = int(input())
-k = int(input())
+find_num = int(input())
+
+r, c, d = 0, 0, 0
+f_r, f_c = 0, 0
 
 snail = [[0] * N for _ in range(N)]
-r, c, d = 0, 0, 0
-ar, ac = 0, 0
 
+# N^2 ~ 1까지 값 뿌리기
 for num in range(N**2, 0, -1):
+    #값 적용
     snail[r][c] = num
 
-    if num == k:
-        ar, ac = r+1, c+1
+    #두번째 문제용
+    if num == find_num:
+        f_r, f_c = r + 1, c + 1
 
+    #다음 좌표 찾기
     nr, nc = r + dr[d], c + dc[d]
 
+    #맞는 방향인지 체크, 아니라면 방향 전환
     if nr < 0 or nr >= N or nc < 0 or nc >= N or snail[nr][nc] != 0:
-        d = (d+1) % 4
+        d = ( d + 1 ) % 4
         nr, nc = r + dr[d], c + dc[d]
 
     r, c = nr, nc
@@ -28,4 +31,4 @@ for num in range(N**2, 0, -1):
 for ans in snail:
     print(*ans)
 
-print(ar, ac)
+print(f_r, f_c)
