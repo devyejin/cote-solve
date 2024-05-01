bfee16bed57f525a9b021cd3a5972a9b621a13eb
yejin <yejinoh58@gmail.com>
2024-05-02 02:02:00 +0900

    Update 회사에 있는 사람.py
    1. dict 자료구조
    2. set 자료구조

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -11,3 +11,24 @@ for _ in range(N):
         del company[man]
 
 print("\n".join(sorted(company.keys(), reverse=True)))
+
+
+
+# set 자료구조 풀이법
+n = int(input())
+company = set() #entry일 때 저장
+
+for _ in range(n):
+    name, status = input().rstrip().split()
+
+    if status == "enter":
+        company.add(name)
+    elif status == "leave":
+        company.remove(name)
+
+#사전 역순으로 정렬해줘야 함, set -> list로 변환해주기
+ans = list(company)
+ans.sort(reverse=True) #원본 자체를 정렬
+
+for i in range(len(ans)):
+    print(ans[i])
