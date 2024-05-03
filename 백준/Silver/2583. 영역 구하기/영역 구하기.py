dbfb8bf2ba76ad1a11cf1dc1ccd0bf9db70035fc
yejin <yejinoh58@gmail.com>
2024-05-03 23:46:38 +0900

    [Silver I] Title: 영역 구하기, Time: 56 ms, Memory: 32748 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+m, n, k = map(int, input().split())
+arr = [[0] * m for _ in range(n)]
+
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):   
+        for j in range(y1, y2): 
+            arr[i][j] = 1
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    arr[x][y] = 1 
+    cnt = 1
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 0:
+            cnt += dfs(nx, ny) 
+    return cnt
+
+answer = []
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 0:
+            answer.append(dfs(i, j))
+
+answer.sort()
+print(len(answer))
+for i in answer:
+    print(i, end=" ")
