d97144e91ad9ced6fb15c76db4da4fa91f18f8ef
yejin <yejinoh58@gmail.com>
2024-05-05 14:09:29 +0900

    Update 3월에 태어난 여성 회원 목록 출력하기.sql

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -10,3 +10,7 @@ AND EXTRACT(MONTH FROM DATE_OF_BIRTH) = '03'
 AND GENDER = 'W'
 AND TLNO IS NOT NULL
 ORDER BY MEMBER_ID;
+
+-- 특정 월만 추출하는거라, 좌변 가공이 불가피해 고민이었음
+-- TO_CHAR의 경우 인덱스가 있는 경우 안타지만, EXTRACT( ) 의 경우 탄다고 함.
+-- TO_CHAR보다는 EXTRACT 사용하기!, 대신 EXTRACT의 경우 시간단위에 대해서는 불가
