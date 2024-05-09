874ad97613d13843544f3d917d0b37227cb71723
yejin <yejinoh58@gmail.com>
2024-05-10 07:28:01 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -2,14 +2,18 @@
 -- SHIPMENT_ID : 아이스크림 공장 -> 아이스크림가게 출하 번호 (JULY테이블 FK, PK)
 -- 7월 맛 별로 총 주문량 JOIN 상반기 맛 별 총 주문량
 
-SELECT
-    FLAVOR
-FROM
-    (
-        SELECT * FROM JULY
-        UNION ALL
-        SELECT * FROM FIRST_HALF
-    )  U
-GROUP BY U.FLAVOR
-ORDER BY SUM(TOTAL_ORDER) DESC
-FETCH FIRST 3 ROWS ONLY;
+SELECT FLAVOR
+FROM (
+    SELECT FLAVOR,
+           ROW_NUMBER() OVER (ORDER BY TOTAL_ORDER DESC) AS rn
+    FROM (
+        SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+        FROM (
+            SELECT * FROM JULY
+            UNION ALL
+            SELECT * FROM FIRST_HALF
+        ) U
+        GROUP BY FLAVOR
+    )
+)
+WHERE rn <= 3;
