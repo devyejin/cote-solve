fbb8ace1a4750da922502d89c3db51054efb6713
yejin <yejinoh58@gmail.com>
2024-06-02 00:45:03 +0900

    Update 3379-score-of-a-string.py

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -6,3 +6,15 @@ class Solution:
         for i in range(len(asci_list) - 1):
             result += abs(asci_list[i] - asci_list[i+1])
         return result
+
+
+
+
+#더 효율적인 풀이법
+#굳이 리스트에 담지 않고 바로 변환해서 계산
+class Solution:
+    def scoreOfString(self, s: str) -> int:  
+        result = 0
+        for i in range(len(s) - 1):
+            result += abs(ord(s[i] - ord(s[i+1])))
+        return result
