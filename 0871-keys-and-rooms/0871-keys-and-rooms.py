a7ae7a9a2d7c9997fa882130219069d74bd073a5
yejin <yejinoh58@gmail.com>
2024-06-03 00:47:39 +0900

    Time: 74 ms (11.65%), Space: 17 MB (90.63%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,15 +1,16 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = [] #방문 기록용
+        visited = set()  #방문 기록용
         
         # v에 연결되어 있는 모든 vertex에 방문, recursive로 구현
         def dfs(v):
-            visited.append(v)
-            for next_v in rooms[v]:
+            visited.add(v)
+            for next_v in rooms[v]: # list -> set O(n) -> O(1) 으로 확인
                 #무한루프 방지
                 if next_v not in visited:
                     dfs(next_v)
             
+        
         dfs(0)
         if len(visited) == len(rooms):
             return True
