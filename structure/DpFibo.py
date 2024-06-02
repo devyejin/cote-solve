8bb8b642b9e2b8b8b734f8a4f9d6789dab00982e
yejin <yejinoh58@gmail.com>
2024-06-03 02:40:49 +0900

    .

---
+++ structure/DpFibo.py
@@ -0,0 +1,12 @@
+#Top down 방식 + memoization 활용
+
+memo = {}
+
+def fibo(n):
+    if n == 1 or n == 2: #base condition
+        return 1
+    if n not in memo:
+        memo[n] = fibo(n-1) + fibo(n-2)
+    return memo[n]
+
+print(fibo(5))
