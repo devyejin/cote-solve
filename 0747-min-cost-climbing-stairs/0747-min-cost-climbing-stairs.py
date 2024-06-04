83a1a27633463eb8a522da37f47f050e98fd2c4f
yejin <yejinoh58@gmail.com>
2024-06-05 08:05:06 +0900

    Time: 57 ms (42.48%), Space: 16.6 MB (76.61%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,13 +4,11 @@ class Solution:
         memo = [-1] * (n+1) #memo[i] : i번째 top에 도달하기 위해 지불해야 하는 최소비용
         memo[0] = 0
         memo[1] = 0
+
+        #bottom up으로 밑에서부터 채워간다면
         
-        def minCost(n:int) -> int: # n번째 top에 도달하기 위해 지불해야 하는 최소비용
+        for i in range(2, n+1):
+            memo[i] = min(memo[i-1] + cost[i-1]  , memo[i-2] + cost[i-2] )
 
-            if memo[n] == -1:
-                memo[n] = min(minCost(n-1) + cost[n-1], minCost(n-2) + cost[n-2])
-
-            return memo[n]
-
-        return minCost(n)
+        return memo[n]
 
