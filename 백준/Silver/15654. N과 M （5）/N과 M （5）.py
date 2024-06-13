6680881078c526771b67699b1d3109b01d1e3421
yejin <yejinoh58@gmail.com>
2024-06-14 05:06:26 +0900

    [Silver III] Title: N과 M (5), Time: 212 ms, Memory: 36580 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -4,15 +4,20 @@ N_list = sorted(list(map(int, input().split())))
 #N : 리스트 길이, M:수열 길이
 def solution(N,M):
     ans = []
+    visited = {num : False for num in N_list} 
+    
     def backtracking(curr):
         if len(curr) == M:
             ans.append(curr[:])
             return
         for num in N_list:
-            if num not in curr:
+            # if num not in curr: # O(n)
+            if not visited[num]: #O(1) 으로 단축
                 curr.append(num)
+                visited[num] = True
                 backtracking(curr)
                 curr.pop()
+                visited[num] = False
     
     backtracking(curr=[])
     return ans
