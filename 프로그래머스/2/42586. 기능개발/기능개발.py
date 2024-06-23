5518b970173c919e366704dc48fb5b205ae615df
yejin <yejinoh58@gmail.com>
2024-06-23 18:54:01 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,22 @@
+import math
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    days = deque()
+    
+    for i in range(len(progresses)):
+        days.append(math.ceil((100 - progresses[i]) / speeds[i]) )
+        # print(days)
+        
+    while days:
+        current = days.popleft()
+        count = 1
+        
+        while days and days[0] <= current: #idx갱신되니까
+            days.popleft()
+            count += 1
+
+        answer.append(count)
+    
+    return answer
