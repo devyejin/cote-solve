7d9241fc3829889995aaa362449e34462ea75a42
yejin <yejinoh58@gmail.com>
2024-06-25 01:44:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 118.74 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,18 +1,6 @@
-from collections import deque
-
 def solution(arr):
-    arr = deque(arr)
-    trans_arr = []
-    
-    #현재값과 이전값을 비교
-    prev = None
-    
-    while arr:
-        current = arr.popleft()
-        
-        if prev is None or current != prev: #첫 번째 요소인 경우 or prev랑 다른 경우
-            trans_arr.append(current)
-        
-        prev = current
-        
-    return trans_arr
+    answer = []
+    for num in arr:
+        if len(answer) == 0 or answer[-1] != num: # -1인덱스로 마지막 요소 접근 
+            answer.append(num)
+    return answer
