504dc9ea7bfd0986ba8a6ee42136d066a90f67d5
yejin <yejinoh58@gmail.com>
2024-06-27 02:48:35 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,13 @@
+import sys
+
+
 stack = []
 
 K = int(input())
 for _ in range(K):
-    num = int(input())
+    num = int(sys.stdin.readline())
     if num == 0:
         stack.pop()
     else:
         stack.append(num)
-
 print(sum(stack))
-    
