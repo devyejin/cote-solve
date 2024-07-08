cf86f4817f255f2974d367be7111cb2175e39507
yejin <yejinoh58@gmail.com>
2024-07-08 16:33:48 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,9 +1,19 @@
+data = input()
+count0 = 0 #0으로 바꾸는 경우
+count1 = 0 #1로 바꾸는 경우
 
-datas = input()
+#idx=0은 prev값이 없으니까 초기값은 직접 체크
+if data[0] == '0':
+    count1 += 1
+else:
+    count0 += 1
 
-cnt_change = 0
-for i in range(len(datas) -1):
-    if datas[i] != datas[i+1]:
-        cnt_change += 1
+#나머지부분 check, prev랑 값이 다르면 변경되는 지점
+for i in range(len(data) - 1):
+    if data[i] != data[i+1]:
+        if data[i+1] == '0':
+            count1 += 1
+        else:
+            count0 += 1
 
-print((cnt_change+1)//2)
+print(min(count0, count1))
