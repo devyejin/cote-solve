667bd008d54da1284acfe3d16cc9fb73858be02e
yejin <yejinoh58@gmail.com>
2024-08-12 16:09:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.84 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.66 ms
+메모리: 10.1 MB, 시간: 0.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 15:57:18
+2024년 08월 12일 16:09:54
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -3,10 +3,10 @@ def solution(numbers):
     result = set()
     n = len(numbers)
         
-    for i in range(n-1):
+    for i in range(n):
         for j in range(i+1, n):
             result.add(numbers[i] + numbers[j]) # O(n^2)
 
-    answer = sorted(list(result)) # O(nlogn) => end up O(nlogn)
+    answer = sorted(list(result)) # O(nlogn) 안에 O(n)이 발생하니  O(n^2logn)
     
     return answer
