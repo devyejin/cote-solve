5a6616df37b88422222d2b046da0b68d09bdd7ea
yejin <yejinoh58@gmail.com>
2024-10-24 00:02:14 +0900

    [Bronze II] Title: Strfry, Time: 160 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,19 +1,11 @@
+#Counter 이용한 방법
+from collections import Counter
+
 N = int(input())
 for _ in range(N):
-  result = 'Possible'
   first, second = input().split()
 
-  first_tbl, second_tbl = [0 for _ in range(26)], [0 for _ in range(26)]
-
-  for ch in first:
-    first_tbl[ord(ch)-ord('a')] += 1
-
-  for ch in second:
-    second_tbl[ord(ch)-ord('a')] += 1
-
-  for i in range(len(first_tbl)):
-    #값이 다르다면 Impossible 반환
-    if first_tbl[i] != second_tbl[i]:
-      result = 'Impossible'
-  
-  print(result)
+  if Counter(first) == Counter(second):
+    print('Possible')
+  else:
+    print('Impossible')
